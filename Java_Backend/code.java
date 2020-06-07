bee9bf58334f709e6cf08f7d7a310822f65f96e3
ishangupta-ds <ishangupta.ds@gmail.com>
2020-06-07 21:17:29 +0530

    Updates

---
+++ Java_Backend/code.java
@@ -0,0 +1,36 @@
+import java.io.*;
+class code
+{
+    public static void main(String[] args) throws IOException {
+        File in = new File("opcodes.kdr");
+
+        String line;
+        
+
+        BufferedReader br = new BufferedReader(new FileReader(in));
+        string arr[];
+        int i=0;
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            String sam =st.nextToken();
+            arr[i++]=sam;
+        }
+        String oneline;
+
+        pw.close();
+        for(int j=0;j<i;j++)
+        {
+            oneline=arr[j];
+            
+                StringTokenizer oneline = new StringTokenizer(oline);
+                String s =st.nextToken();
+                if(
+
+            }
+        }
+    }
+    catch(IOException e){
+        System.out.println("error"+e);
+    }
+}
+}
